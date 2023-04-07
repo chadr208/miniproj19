ec5b7adcbad7241947c2180743999322895a8009
Chad Rajcooar <chadr208@gmail.com>
2023-04-07 12:27:35 -0400

    commit to push

---
+++ Develop/client/webpack.config.js
@@ -0,0 +1,66 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { GenerateSW } = require('workbox-webpack-plugin');
+
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js',
+      cards: './src/js/cards.js'
+    },
+
+    // TODO: Add the correct output
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+
+    // TODO: Add the correct plugins
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'TODOs List'
+      }),
+
+      new GenerateSW(),
+      new WebpackPwaManifest({
+        name: "App",
+        short_name: "MyPWA",
+        background_color: '#FFFFFF',
+        display: 'standalone',
+        orientation: 'portrait',
+        fingerprints: 'false',
+        publicPath: './',
+        icons: {
+          src: path.resolve('src/images/logo.png'),
+          sizes: [120, 152, 167, 180, 1024],
+        },
+      }),
+
+    ],
+
+    // TODO: Add the correct modules
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
+      ],
+    }
+  };
+};
